8408e2d40465e7994de01482eefc1edebaa21536
Guilherme Vandanezi <guilhermevandanezi@outlook.com>
2025-05-17 17:13:40 -0300

    refactor blog

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,8 @@
 import { Inter, PT_Sans_Caption } from "next/font/google";
-import { Header } from "./header";
+
 import { Footer } from "./footer";
+import { Header } from "./header";
+import { CallToAction } from "@/templates/landing-page/sections";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -24,6 +26,7 @@ export const Layout = ({ children }: LayoutProps) => {
     >
       <Header />
       <main className="flex-1 flex flex-col mb-12">{children}</main>
+      <CallToAction />
       <Footer />
     </div>
   );
